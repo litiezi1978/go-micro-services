67fcb53ad88d914505f2793f063c6389d3375c0c
litie <1794703060@qq.com>
2020-03-01 18:11:47 +0800

    recomm for sidecar model

--- cmd/recommendation/main.go
+++ cmd/recommendation/main.go
@@ -12,15 +12,21 @@ import (
 )
 
 func main() {
+	host_ip := os.Getenv("hostIP")
 	serv_ip := os.Getenv("serverIP")
 	serv_port, _ := strconv.Atoi(os.Getenv("serverPort"))
-	mongoAddr := os.Getenv("mongoAddr")
-	jaegerAddr := os.Getenv("jaegerAddr")
-	consulAddr := os.Getenv("consulAddr")
 	consul_check_port, _ := strconv.Atoi(os.Getenv("consulCheckPort"))
+	consulPort := os.Getenv("consulPort")
+	jaegerPort := os.Getenv("jaegerPort")
 
-	fmt.Printf("recommendation ip = %s, port = %d\n", serv_ip, serv_port)
+	consulAddr := host_ip + ":" + consulPort
+	fmt.Printf("init consul with addr: %s\n", consulAddr)
+	registry, err := registry.NewClient(consulAddr)
+	if err != nil {
+		log.Fatalf("failed to init consul, err=%v", err)
+	}
 
+	jaegerAddr := host_ip + ":" + jaegerPort
 	fmt.Printf("init distributed tracing with addr: %s\n", jaegerAddr)
 	tracer, closer, err := tracing.Init("recommendation", jaegerAddr)
 	if err != nil {
@@ -28,18 +34,17 @@ func main() {
 	}
 	defer closer.Close()
 
-	fmt.Printf("init consul with addr: %s\n", consulAddr)
-	registry, err := registry.NewClient(consulAddr)
+	mongoAddr, err := registry.FindService("srv-mongo-recomm")
 	if err != nil {
-		log.Fatalf("failed to init consul, err=%v", err)
+		log.Fatalf("failed to search srv-mongo-rate from Consul, %v", err)
 	}
-
-	fmt.Printf("init mongo DB with addr: %s\n", mongoAddr)
+	log.Printf("init mongo DB with addr: %s\n", mongoAddr)
 	mongoClient, err := recommendation.InitializeDatabase(mongoAddr)
 	if err != nil {
 		log.Fatalf("failed to init mongo, err=%v", err)
 	}
 
+	fmt.Printf("recommendation ip = %s, port = %d\n", serv_ip, serv_port)
 	srv := &recommendation.Server{
 		Tracer:       tracer,
 		Registry:     registry,
